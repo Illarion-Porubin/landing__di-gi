4948ee53560b3104793a97ced477f6649b20ef1a
Illarion Porubin <geniuslar@mail.ru>
2020-10-03 21:35:18 +0300

    Di-Gi-footer

--- app/js/main.js
+++ app/js/main.js
@@ -20,6 +20,27 @@ $(function(){
       }
     ]
   });
+  $('.marketing__slider').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: false,
+    prevArrow: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+        
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          arrows:false
+        }
+      }
+    ]
+  });
 
   
 });
